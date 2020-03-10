cc090daefeb601090605e7a7b459f69de1416c8d
Kevin Sun <kev.sun78@gmail.com>
2020-03-09 23:40:44 -0700

    better formatting

--- src/party.js
+++ src/party.js
@@ -37,20 +37,19 @@ function sleep(ms) {
 var mapping = {"BROOKLYN": 0, "MANHATTAN": 0, "BRONX": 0, "QUEENS": 0, "STATEN ISLAND": 0};              
 
 // this gets it done
-async function calc() {
+// async function calc() {
     d3.csv(partycsv).then(function(data){
-        data.forEach(function(d){
-            if (Object.keys(mapping).includes(d.Borough)) {
-                mapping[d.Borough]++;
-            }
-        })
-    });
 
-    // must sleep!! zzzzzz....
-    await sleep(600);
+        for (let step = 0; step < 5; step++) {
+            var boroughCategory = data[step].Borough2;
+            var count = data[step].Incidents;
+            mapping[boroughCategory] = count;
+        }
+        
+        console.log(mapping);
 
-    // Add the SVG Text Element to the svgContainer
-    var text = svg.selectAll(".labels")
+        // Add the SVG Text Element to the svgContainer
+        var text = svg.selectAll(".labels")
         .data(d3.entries(mapping))
         .enter()
         .append("text")
@@ -62,60 +61,58 @@ async function calc() {
             return d.key;
         });
 
-    // Add left circles (land area not in consideration)
-    svg.selectAll(".borough")
-        .data(d3.entries(mapping))
-        .enter()
-        .append("circle")
-        .style("stroke", "gray")
-        .style("fill", CIRCLE_FILL)
-        .attr("r", function(d){
-            return Math.sqrt(d.value) / RAW_CIRCLE_ADJUSTMENT;
-        })
-        .attr("cx", XSTART + TEXT_RAW_CIRCLE_DIST)
-        .attr("cy", function (d, i) {
-            return i * YSPACING_SCALE + YSPACING_OFFSET;
-        })
-        .append("svg:title")
-        .text(function(d) {
-            return d.key + ": " + d.value + " noise complaints"
-        });
+        // Add left circles (land area not in consideration)
+        svg.selectAll(".borough")
+            .data(d3.entries(mapping))
+            .enter()
+            .append("circle")
+            .style("stroke", "gray")
+            .style("fill", CIRCLE_FILL)
+            .attr("r", function(d){
+                return Math.sqrt(d.value) / RAW_CIRCLE_ADJUSTMENT;
+            })
+            .attr("cx", XSTART + TEXT_RAW_CIRCLE_DIST)
+            .attr("cy", function (d, i) {
+                return i * YSPACING_SCALE + YSPACING_OFFSET;
+            })
+            .append("svg:title")
+            .text(function(d) {
+                return d.key + ": " + d.value + " noise complaints"
+            });
 
-    // square mileage
-    var areas = {"BROOKLYN": 69.5, "MANHATTAN": 22.8, "BRONX": 42.5, "QUEENS": 108.1, "STATEN ISLAND": 58.69};  
+        // square mileage
+        var areas = {"BROOKLYN": 69.5, "MANHATTAN": 22.8, "BRONX": 42.5, "QUEENS": 108.1, "STATEN ISLAND": 58.69};  
 
-    // add right circles, land area taken into consideration
-    // divide each circle area by land area and resize
-    svg2.selectAll(".borough")
-        .data(d3.entries(mapping))
-        .enter()
-        .append("circle")
-        .style("stroke", "gray")
-        .style("fill", CIRCLE_FILL)
-        .attr("r", function(d){
-            return Math.sqrt(d.value / areas[d.key]) * PROCESSED_CIRCLE_ADJUSTMENT;
-        })
-        .attr("cx", RIGHT_CIRCLES_X)
-        .attr("cy", function (d, i) {
-            return i * YSPACING_SCALE + YSPACING_OFFSET;
-        })
-        .append("svg:title")
-        .text(function(d) {
-            return d.key + ": " + Math.round(d.value / areas[d.key]) + " noise complaints per square mile"
-        });
+        // add right circles, land area taken into consideration
+        // divide each circle area by land area and resize
+        svg2.selectAll(".borough")
+            .data(d3.entries(mapping))
+            .enter()
+            .append("circle")
+            .style("stroke", "gray")
+            .style("fill", CIRCLE_FILL)
+            .attr("r", function(d){
+                return Math.sqrt(d.value / areas[d.key]) * PROCESSED_CIRCLE_ADJUSTMENT;
+            })
+            .attr("cx", RIGHT_CIRCLES_X)
+            .attr("cy", function (d, i) {
+                return i * YSPACING_SCALE + YSPACING_OFFSET;
+            })
+            .append("svg:title")
+            .text(function(d) {
+                return d.key + ": " + Math.round(d.value / areas[d.key]) + " noise complaints per square mile"
+            });
+
+    });
+    
 
 
-    // //Add SVG Text Element Attributes
-    // var textLabels = text
-    //              .attr("x", function(d) { return d.cx; })
-    //              .attr("y", function(d) { return d.cy; })
-    //              .text( function (d) { return "( " + d.cx + ", " + d.cy +" )"; })
-    //              .attr("font-family", "sans-serif")
-    //              .attr("font-size", "20px")
-    //              .attr("fill", "red");
-}
+    // must sleep!! zzzzzz....
+    // await sleep(2000);
 
-calc();
+    
+
+// calc();
 
 $("#right-circles").hide();
 $(".right-circle-title").hide();
