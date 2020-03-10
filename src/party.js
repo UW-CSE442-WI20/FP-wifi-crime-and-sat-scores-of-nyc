09b422c837afb5d5e44244221a543cf6a05a8545
Kevin Sun <kev.sun78@gmail.com>
2020-03-10 02:03:30 -0700

    anchor counts text to center of circles

--- src/party.js
+++ src/party.js
@@ -4,8 +4,6 @@ require("babel-polyfill");
 import partycsv from "./party_in_nyc.csv";
 
 const XSTART = 0;
-const X_ON_LEFT_CIRCLES = 220;
-const X_ON_RIGHT_CIRCLES = 140;
 
 const YSPACING_SCALE = 160;
 const YSPACING_OFFSET = 100;
@@ -14,25 +12,22 @@ const RAW_CIRCLE_ADJUSTMENT = 4;
 const PROCESSED_CIRCLE_ADJUSTMENT = 1.75;
 
 const TEXT_RAW_CIRCLE_DIST = 230;
-const RIGHT_CIRCLES_X = 150;
 
 const SVG_WIDTH = 350;
 const SVG_HEIGHT = 800;
 
-const CIRCLE_FILL = "steelblue";
-
 // Make left side SVG Container
 var svg = d3.select("#viz")
-                    .append("svg")
-                    .attr("width", SVG_WIDTH)
-                    .attr("height", SVG_HEIGHT)
-                    .attr("transform", "translate(0,0)"); // todo: try deleting this see if anything changes
+    .append("svg")
+    .attr("width", 300)
+    .attr("height", SVG_HEIGHT)
+    .attr("transform", "translate(0,0)"); // todo: try deleting this see if anything changes
 
 var svg2 = d3.select("#viz")
-                    .append("svg")
-                    .attr("width", SVG_WIDTH)
-                    .attr("height", SVG_HEIGHT)
-                    .attr("id", "right-circles");
+    .append("svg")
+    .attr("width", SVG_WIDTH)
+    .attr("height", SVG_HEIGHT)
+    .attr("id", "right-circles");
                     
 var mapping = {"BROOKLYN": 0, "MANHATTAN": 0, "BRONX": 0, "QUEENS": 0, "STATEN ISLAND": 0};
 var areas = {"BROOKLYN": 69.5, "MANHATTAN": 22.8, "BRONX": 42.5, "QUEENS": 108.1, "STATEN ISLAND": 58.69};  
@@ -47,85 +42,91 @@ d3.csv(partycsv).then(function(data){
     }
 
     // Add the SVG Text Element to the svgContainer
-    var text = svg.selectAll(".labels")
-    .data(d3.entries(mapping))
-    .enter()
-    .append("text")
-    .attr("x", XSTART)
-    .attr("y", function(d, i) {
-        return i * YSPACING_SCALE + YSPACING_OFFSET;
-    })
-    .text(function(d){
-        return d.key;
-    });
-
-    // Add left circles (land area not in consideration)
-    svg.selectAll(".borough")
+    svg.selectAll(".labels")
         .data(d3.entries(mapping))
         .enter()
-        .append("circle")
-        .style("fill", function(d, i) {
-            return colors[i];
+        .append("text")
+        .attr("x", XSTART)
+        .attr("y", function(d, i) {
+            return i * YSPACING_SCALE + YSPACING_OFFSET;
         })
-        .attr("r", function(d){
+        .text(function(d){
+            return d.key;
+        });
+
+    var nodes = svg.append("g")
+        .attr("class", "nodes")
+        .selectAll("circle")
+        .data(d3.entries(mapping))
+        .enter()
+        // Add one g element for each data node here.
+        .append("g")
+        // Position the g element like the circle element used to be.
+        .attr("transform", function(d, i) {
+            // Set d.x and d.y here so that other elements can use it. d is 
+            // expected to be an object here.
+            d.x = XSTART + TEXT_RAW_CIRCLE_DIST;
+            d.y = i * YSPACING_SCALE + YSPACING_OFFSET
+            return "translate(" + d.x + "," + d.y + ")"; 
+        });
+
+    // Add a circle element to the previously added g element.
+    nodes.append("circle")
+        .attr("class", "node")
+        .attr("r", function(d) {
             return Math.sqrt(d.value) / RAW_CIRCLE_ADJUSTMENT;
         })
-        .attr("cx", XSTART + TEXT_RAW_CIRCLE_DIST)
-        .attr("cy", function (d, i) {
-            return i * YSPACING_SCALE + YSPACING_OFFSET;
+        .style("fill", function(d, i) {
+            return colors[i];
         })
         .append("svg:title")
         .text(function(d) {
             return d.key + ": " + d.value + " noise complaints"
         });
 
-    // left circle counts
-    var text = svg.selectAll(".labels")
-    .data(d3.entries(mapping))
-    .enter()
-    .append("text")
-    .attr("x", X_ON_LEFT_CIRCLES)
-    .attr("y", function(d, i) {
-        return i * YSPACING_SCALE + YSPACING_OFFSET + 5;
-    })
-    .text(function(d){
-        return Math.round(d.value / 1000) + "k";
-    });
+    // Add a text element to the previously added g element.
+    nodes.append("text")
+        .attr("text-anchor", "middle")
+        .text(function(d) {
+            return Math.round(d.value / 1000) + "k";
+        });
 
-    // add right circles, land area taken into consideration
-    // divide each circle area by land area and resize
-    svg2.selectAll(".borough")
+    var nodes2 = svg2.append("g")
+        .attr("class", "nodes")
+        .selectAll("circle")
         .data(d3.entries(mapping))
         .enter()
-        .append("circle")
+        // Add one g element for each data node here.
+        .append("g")
+        // Position the g element like the circle element used to be.
+        .attr("transform", function(d, i) {
+        // Set d.x and d.y here so that other elements can use it. d is 
+        // expected to be an object here.
+         d.x = XSTART + TEXT_RAW_CIRCLE_DIST;
+         d.y = i * YSPACING_SCALE + YSPACING_OFFSET
+         return "translate(" + d.x + "," + d.y + ")"; 
+        });
+
+    // Add a circle element to the previously added g element.
+    nodes2.append("circle")
+        .attr("class", "node")
+        .attr("r", function(d) {
+            return Math.sqrt(d.value / areas[d.key]) * PROCESSED_CIRCLE_ADJUSTMENT;
+        })
         .style("fill", function(d, i) {
             return colors[i];
         })
-        .attr("r", function(d){
-            return Math.sqrt(d.value / areas[d.key]) * PROCESSED_CIRCLE_ADJUSTMENT;
-        })
-        .attr("cx", RIGHT_CIRCLES_X)
-        .attr("cy", function (d, i) {
-            return i * YSPACING_SCALE + YSPACING_OFFSET;
-        })
         .append("svg:title")
         .text(function(d) {
-            return d.key + ": " + Math.round(d.value / areas[d.key]) + " noise complaints per square mile"
-    });
-
-    // right circle counts
-    svg2.selectAll(".labels")
-        .data(d3.entries(mapping))
-        .enter()
-        .append("text")
-        .attr("x", X_ON_RIGHT_CIRCLES)
-        .attr("y", function(d, i) {
-            return i * YSPACING_SCALE + YSPACING_OFFSET + 5;
-        })
-        .text(function(d, i){
-            return Math.round(d.value / areas[d.key]);
+            return d.key + ": " + Math.round(d.value / areas[d.key]) + " noise complaints per square mile";
         });
 
+    // Add a text element to the previously added g element.
+    nodes2.append("text")
+        .attr("text-anchor", "middle")
+        .text(function(d) {
+            return Math.round(d.value / areas[d.key]);
+        });
 
 });
 
--- src/weefee.js
+++ src/weefee.js
@@ -1,8 +1,6 @@
 import weefee from "./weefee.csv";
 
 const XSTART = 0;
-const X_ON_LEFT_CIRCLES = 220;
-const X_ON_RIGHT_CIRCLES = 145;
 
 const YSPACING_SCALE = 160;
 const YSPACING_OFFSET = 100;
@@ -11,7 +9,6 @@ const RAW_CIRCLE_ADJUSTMENT = 2.5;
 const PROCESSED_CIRCLE_ADJUSTMENT = 16;
 
 const TEXT_RAW_CIRCLE_DIST = 230;
-const RIGHT_CIRCLES_X = 150;
 
 const SVG_WIDTH = 350;
 const SVG_HEIGHT = 800;
@@ -19,7 +16,7 @@ const SVG_HEIGHT = 800;
 // Make left side SVG Container
 var svg3 = d3.select("#viz2")
                     .append("svg")
-                    .attr("width", SVG_WIDTH)
+                    .attr("width", 320)
                     .attr("height", SVG_HEIGHT)
                     .attr("transform", "translate(0,0)"); // todo: try deleting this see if anything changes
 
@@ -41,89 +38,92 @@ d3.csv(weefee).then(function(data) {
         }
     });
 
-    console.log(mapping);
-
     // Add the SVG Text Element to the svgContainer
     var text = svg3.selectAll(".labels")
-    .data(d3.entries(mapping))
-    .enter()
-    .append("text")
-    .attr("x", XSTART)
-    .attr("y", function(d, i) {
-        return i * YSPACING_SCALE + YSPACING_OFFSET;
-    })
-    .text(function(d){
-        return d.key.toUpperCase();
-    });
-
-    // Add left circles (land area not in consideration)
-    svg3.selectAll(".borough")
-        .data(d3.entries(mapping))
-        .enter()
-        .append("circle")
-        .style("fill", function(d, i) {
-            return colors[i];
-        })
-        .attr("r", function(d){
-            return Math.sqrt(d.value) * RAW_CIRCLE_ADJUSTMENT;
-        })
-        .attr("cx", XSTART + TEXT_RAW_CIRCLE_DIST)
-        .attr("cy", function (d, i) {
-            return i * YSPACING_SCALE + YSPACING_OFFSET;
-        })
-        .append("svg:title")
-        .text(function(d) {
-            return d.key + ": " + d.value + " noise complaints"
-        });
-
-    // left circle counts
-    var text = svg3.selectAll(".labels")
-    .data(d3.entries(mapping))
-    .enter()
-    .append("text")
-    .attr("x", X_ON_LEFT_CIRCLES)
-    .attr("y", function(d, i) {
-        return i * YSPACING_SCALE + YSPACING_OFFSET + 5;
-    })
-    .text(function(d){
-        return Math.round(d.value);
-    });
-
-    // add right circles, land area taken into consideration
-    // divide each circle area by land area and resize
-    svg4.selectAll(".borough")
-        .data(d3.entries(mapping))
-        .enter()
-        .append("circle")
-        .style("fill", function(d, i) {
-            return colors[i];
-        })
-        .attr("r", function(d){
-            return Math.sqrt(d.value / areas[d.key]) * PROCESSED_CIRCLE_ADJUSTMENT;
-        })
-        .attr("cx", RIGHT_CIRCLES_X)
-        .attr("cy", function (d, i) {
-            return i * YSPACING_SCALE + YSPACING_OFFSET;
-        })
-        .append("svg:title")
-        .text(function(d) {
-            return d.key + ": " + Math.round(d.value / areas[d.key]) + " noise complaints per square mile"
-    });
-
-    // right circle counts
-    svg4.selectAll(".labels")
         .data(d3.entries(mapping))
         .enter()
         .append("text")
-        .attr("x", X_ON_RIGHT_CIRCLES)
+        .attr("x", XSTART)
         .attr("y", function(d, i) {
-            return i * YSPACING_SCALE + YSPACING_OFFSET + 5;
+            return i * YSPACING_SCALE + YSPACING_OFFSET;
         })
-        .text(function(d, i){
-            return Math.round(d.value / areas[d.key]);
+        .text(function(d){
+            return d.key.toUpperCase();
         });
 
+    var nodes3 = svg3.append("g")
+        .attr("class", "nodes")
+        .selectAll("circle")
+        .data(d3.entries(mapping))
+        .enter()
+        // Add one g element for each data node here.
+        .append("g")
+        // Position the g element like the circle element used to be.
+        .attr("transform", function(d, i) {
+            // Set d.x and d.y here so that other elements can use it. d is 
+            // expected to be an object here.
+            d.x = XSTART + TEXT_RAW_CIRCLE_DIST;
+            d.y = i * YSPACING_SCALE + YSPACING_OFFSET
+            return "translate(" + d.x + "," + d.y + ")"; 
+        });
 
+    // Add a circle element to the previously added g element.
+    nodes3.append("circle")
+        .attr("class", "node")
+        .attr("r", function(d) {
+            return Math.sqrt(d.value) * RAW_CIRCLE_ADJUSTMENT;
+        })
+        .style("fill", function(d, i) {
+            return colors[i];
+        })
+        .append("svg:title")
+        .text(function(d) {
+            return d.key + ": " + d.value + " wifi hotspots"
+        });
+
+    // Add a text element to the previously added g element.
+    nodes3.append("text")
+        .attr("text-anchor", "middle")
+        .text(function(d) {
+            return Math.round(d.value);
+        });
+
+    var nodes4 = svg4.append("g")
+        .attr("class", "nodes")
+        .selectAll("circle")
+        .data(d3.entries(mapping))
+        .enter()
+        // Add one g element for each data node here.
+        .append("g")
+        // Position the g element like the circle element used to be.
+        .attr("transform", function(d, i) {
+        // Set d.x and d.y here so that other elements can use it. d is 
+        // expected to be an object here.
+         d.x = XSTART + TEXT_RAW_CIRCLE_DIST;
+         d.y = i * YSPACING_SCALE + YSPACING_OFFSET
+         return "translate(" + d.x + "," + d.y + ")"; 
+        });
+
+    // Add a circle element to the previously added g element.
+    nodes4.append("circle")
+        .attr("class", "node")
+        .attr("r", function(d) {
+            return Math.sqrt(d.value / areas[d.key]) * PROCESSED_CIRCLE_ADJUSTMENT;
+        })
+        .style("fill", function(d, i) {
+            return colors[i];
+        })
+        .append("svg:title")
+        .text(function(d) {
+            return d.key + ": " + Math.round(d.value / areas[d.key]) + " wifi hotspots per square mile";
+        });
+
+    // Add a text element to the previously added g element.
+    nodes4.append("text")
+        .attr("text-anchor", "middle")
+        .text(function(d) {
+            return Math.round(d.value / areas[d.key]);
+        });
 });
 
 $("#right-circles2").hide();
